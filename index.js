981573d29a431ed94fcc7df22960fcec8bc9d6e4
Emiliyan Gospodinov <ego@syngenio.de>
2017-03-19 22:26:46 +0100

    asd

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var app = express();
 app.use(cors());
 
 app.get('/stations',(req, res)=>{
-    request('https://www.seirsanduk.com/',(err,result,body)=>{
+    request('https://www.seirsanduk.com/',function(err,result,body){
        if(err){
            console.log(err);
        }else{
             var page = parser.parse(body);
-            var menu = page.querySelector('ul.nav_vodlist').childNodes.map((node)=>{
+            var menu = page.querySelector('ul.nav_vodlist').childNodes.map(function(node){
                 if(node.childNodes){
                     return {
                         href: node.childNodes[0].rawAttrs.replace(/(href=\")|(\")*$/igm,''),
@@ -20,7 +20,7 @@ app.get('/stations',(req, res)=>{
                         name:node.childNodes[0].childNodes[1].rawText
                     }
                 }
-            }).filter((item)=>{
+            }).filter(function(item){
                 if(item) return item;
             });
 
@@ -33,19 +33,19 @@ app.get('/station', (req,res)=>{
     request({
         url:'http://www.seirsanduk.com/'+req.query.id,
         // headers:req.headers
-    },(error,result,body)=>{
+    },function(error,result,body){
         if(error) {
             console.log(error);
         } else {
             var page = parser.parse(body,{script:true});
             var scripts =page.querySelectorAll('.content script');
-            var url  = scripts.map((script)=>{
+            var url  = scripts.map(function(script){
                 return script.childNodes[0];
-            }).filter((script)=>{
+            }).filter(function(script){
                 if(script) return script;
-            }).map((script)=>{
+            }).map(function(script){
                 return script.rawText;
-            }).filter((script)=>{
+            }).filter(function(script){
                 if(script.indexOf('jwplayer("Element").setup')>0){
                     return script;
                 }
@@ -56,19 +56,17 @@ app.get('/station', (req,res)=>{
     });
 });
 
-app.get('/stream',(req,res,body)=>{
+app.get('/stream',function(req,res,body){
     request({
         url:req.query.url,
         headers:{
             'Referer':'http://seirsanduk.com/'
         }
-    },(error,response,body)=>{
+    },function(error,response,body){
         if(!error){
             res.send(body);
         }
     })
 })
 
-app.listen(80,()=>{
-    
-});
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
